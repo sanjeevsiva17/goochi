009c46e8081725d4d4360a8c0a75d99eea84720e
tushardas <tushar.das5@gmail.com>
2021-05-15 23:38:02 +0530

    refs #2 removed different logging formats

--- log/entry.go
+++ log/entry.go
@@ -10,7 +10,6 @@ type logEntry struct {
 	Time    time.Time              `json:"time"`
 	Message interface{}            `json:"message"`
 	System  map[string]interface{} `json:"system"`
-	Data    map[string]interface{} `json:"data,omitempty"`
 }
 
 func (l *logEntry) TerminalOutput() string {
@@ -18,50 +17,11 @@ func (l *logEntry) TerminalOutput() string {
 
 	s := fmt.Sprintf("\u001B[%dm%s\u001B[0m [%s] ", levelColor, l.Level.String()[0:4], l.Time.Format("15:04:05"))
 
-	if len(l.Data) > 0 {
-		if l.Message != nil { // http client sends message on error
-			s += fmt.Sprintf(" %s", l.Message)
-		} else {
-			//nolint:mnd // 1000 is used to convert the microsecond to milliseconds
-			s += fmt.Sprintf("\u001B[%dm %s\u001B[0m %v - %.2fms", 37, l.Data["method"], l.Data["uri"], l.Data["duration"].(float64)/1000)
-		}
-	} else {
-		s += fmt.Sprintf(" %v", l.Message)
-	}
+	s += fmt.Sprintf(" %v", l.Message)
 
 	if l.System != nil {
 		s += fmt.Sprintf("\n%15s \u001B[%dm (Memory: %v GoRoutines: %v) \u001B[0m", "", 37, l.System["alloc"], l.System["goRoutines"])
 	}
 
 	return s + "\n"
-}
-
-// dataFromStrings takes multiple strings and creates a log logEntry from it.
-// It is written separately so different use-cases can be tested safely in isolation.
-// nolint
-func dataFromStrings(args ...string) *logEntry {
-	l := logEntry{
-		Time: time.Now(),
-		Data: make(map[string]interface{}),
-	}
-
-	// No need for array if size is only 1
-	if len(args) == 1 {
-		j, valMap := isJSON(args[0])
-		if j {
-			l.Data = valMap
-
-			if m, ok := valMap["message"]; ok {
-				l.Message = m.(string)
-
-				delete(valMap, "message")
-			}
-		} else {
-			l.Message = args[0]
-		}
-	} else {
-		l.Message = args
-	}
-
-	return &l
 }
--- log/logger.go
+++ log/logger.go
@@ -1,20 +1,117 @@
 package log
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"os"
+	"strings"
+	"time"
 )
 
-func newLogger() *logger {
+type logger struct {
+	out   io.Writer
+	level level
+	// App Specific Data for the logger
+	isTerminal bool
+}
+
+// log does the actual logging. This function creates the logEntry message and outputs it in color format
+// in terminal context and gives out json in non terminal context. Also, sends to echo if client is present.
+func (l *logger) log(level level, args string) {
+	if l.level < level {
+		return
+	}
+
+	e := &logEntry{
+		Level:   level,
+		Time:    time.Now(),
+		Message: args,
+		System:  fetchSystemStats(),
+	}
+
+	if l.isTerminal {
+		fmt.Fprint(l.out, e.TerminalOutput())
+	} else {
+		_ = json.NewEncoder(l.out).Encode(e)
+	}
+}
+
+func isJSON(s string) (ok bool, hashmap map[string]interface{}) {
+	var js map[string]interface{}
+	return json.Unmarshal([]byte(s), &js) == nil, js
+}
+
+func (l *logger) Log(args ...interface{}) {
+	l.log(Info, fmt.Sprint(args...))
+}
+
+func (l *logger) Logf(format string, args ...interface{}) {
+	l.log(Info, fmt.Sprintf(format, args...))
+}
+
+func (l *logger) Info(args ...interface{}) {
+	l.log(Info, fmt.Sprint(args...))
+}
+
+func (l *logger) Infof(format string, args ...interface{}) {
+	l.log(Info, fmt.Sprintf(format, args...))
+}
+
+func (l *logger) Debug(args ...interface{}) {
+	l.log(Debug, fmt.Sprint(args...))
+}
+
+func (l *logger) Debugf(format string, args ...interface{}) {
+	l.log(Debug, fmt.Sprintf(format, args...))
+}
+
+func (l *logger) Warn(args ...interface{}) {
+	l.log(Warn, fmt.Sprint(args...))
+}
+
+func (l *logger) Warnf(format string, args ...interface{}) {
+	l.log(Warn, fmt.Sprintf(format, args...))
+}
+
+func (l *logger) Error(args ...interface{}) {
+	l.log(Error, fmt.Sprint(args...))
+}
+
+func (l *logger) Errorf(format string, args ...interface{}) {
+	l.log(Error, fmt.Sprintf(format, args...))
+}
+
+func (l *logger) Fatal(args ...interface{}) {
+	l.log(Fatal, fmt.Sprint(args...))
+	os.Exit(1)
+}
+
+func (l *logger) Fatalf(format string, args ...interface{}) {
+	l.log(Fatal, fmt.Sprintf(format, args...))
+	os.Exit(1)
+}
+
+func NewLogger(level string) Logger {
 	l := &logger{
 		out: os.Stdout,
+	}
+
+	switch strings.ToUpper(level) {
+	case "INFO":
+		l.level = Info
+	case "WARN":
+		l.level = Warn
+	case "DEBUG":
+		l.level = Debug
+	case "FATAL":
+		l.level = Fatal
+	case "ERROR":
+		l.level = Error
 	}
 
 	// Set terminal to ensure proper output format.
 	l.isTerminal = checkIfTerminal(l.out)
 
 	return l
-}
-
-func NewLogger() Logger {
-	return newLogger()
 }
